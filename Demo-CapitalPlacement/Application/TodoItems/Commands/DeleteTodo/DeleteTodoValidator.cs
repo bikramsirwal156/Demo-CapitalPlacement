74b2d3e6554e6b32e6ae785eade1be3b0bc53260
Bikram Sirwal <bikram.sirwal@defactoinfotech.com>
2024-05-14 15:34:35 +0530

    Added DeteleTodocammand validator to validate input string from Query

---
+++ Demo-CapitalPlacement/Application/TodoItems/Commands/DeleteTodo/DeleteTodoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Demo_CapitalPlacement.Application.TodoItems.Commands.DeleteTodo
+{
+    public class DeleteTodoValidator : AbstractValidator<DeleteTodoCommand>
+    {
+        public DeleteTodoValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id cannot be empty")
+                              .NotNull().WithMessage("Id cannot be null")
+                            .Must(BeNumericAndNonNegative).WithMessage("Id must be numeric and non-negative");
+        }
+
+        private bool BeNumericAndNonNegative(int id)
+        {
+            return id >= 0;
+        }
+    }
+}
