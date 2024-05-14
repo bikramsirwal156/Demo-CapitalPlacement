a9de9673a8f569f022c7a5c9fe8956bd55052e9e
Bikram Sirwal <bikram.sirwal@defactoinfotech.com>
2024-05-14 13:25:28 +0530

    Added GetTotItemQuery implemetation

--- Demo-CapitalPlacement/Application/TodoItems/Queries/GetTodoItemsQuery.cs
+++ Demo-CapitalPlacement/Application/TodoItems/Queries/GetTodoItemsQuery.cs
@@ -1,6 +1,28 @@
-﻿namespace Demo_CapitalPlacement.Application.TodoItems.Queries
+﻿using Demo_CapitalPlacement.Application.Common.Interfaces;
+using Demo_CapitalPlacement.Application.Common.Models;
+using MediatR;
+
+namespace Demo_CapitalPlacement.Application.TodoItems.Queries
 {
-    public class GetTodoItemsQuery
+    public class GetTodoItemsQuery : IRequest<ResultDto<List<TodoViewModel>>> { }
+    public class Handler(ICpCodeRepository cpCodeRepository) : IRequestHandler<GetTodoItemsQuery, ResultDto<List<TodoViewModel>>>
     {
+        private readonly ICpCodeRepository _cpCodeRepository = cpCodeRepository;
+        public async Task<ResultDto<List<TodoViewModel>>> Handle(GetTodoItemsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _cpCodeRepository.GetAllTodoItemAsync();
+                if (result.IsSuccess)
+                {
+                    return new ResultDto<List<TodoViewModel>>(result.Data, true);
+                }
+                return new ResultDto<List<TodoViewModel>>(result.Data, false, result.Message);
+            }
+            catch (Exception ex)
+            {
+                return new ResultDto<List<TodoViewModel>>(null, false, ex.Message);
+            }
+        }
     }
 }
