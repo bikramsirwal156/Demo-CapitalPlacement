8735f3dedeb0bb1101c8da1cd55954e972e82df7
Bikram Sirwal <bikram.sirwal@defactoinfotech.com>
2024-05-14 15:30:39 +0530

    Added Migrations

---
+++ Demo-CapitalPlacement/Migrations/20240514081939_InitialDatabaseSetup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo_CapitalPlacement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDatabaseSetup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TodoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    RefrenceNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplyDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Experience = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoOptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OptionName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TodoItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoOptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TodoOptions_TodoItems_TodoItemId",
+                        column: x => x.TodoItemId,
+                        principalTable: "TodoItems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoOptions_TodoItemId",
+                table: "TodoOptions",
+                column: "TodoItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoOptions");
+
+            migrationBuilder.DropTable(
+                name: "TodoItems");
+        }
+    }
+}
