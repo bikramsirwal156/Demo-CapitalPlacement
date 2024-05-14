1a9102c6efa37b04e715ed0184f63692aee9b1fd
Bikram Sirwal <bikram.sirwal@defactoinfotech.com>
2024-05-14 13:16:26 +0530

    Added FluentValidator package and added a class for createtodovalidator to validate data

---
+++ Demo-CapitalPlacement/Application/TodoItems/Commands/CreateTodo/CreateTodoCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Demo_CapitalPlacement.Application.TodoItems.Commands.CreateTodo
+{
+    public class CreateTodoCommandValidator : AbstractValidator<CreateTodoCommand>
+    {
+        public CreateTodoCommandValidator()
+        {
+            RuleFor(x => x.Description).NotNull().NotEmpty().WithMessage("Job description cannot be empty");
+            RuleFor(x => x.IsActive).NotNull()
+            .NotEmpty().WithMessage("IsActive must not be empty.");
+            RuleFor(x => x.RefrenceNumber).NotEmpty().NotNull().WithMessage("Refrence Id cannot be empty");
+            RuleFor(x => x.ApplyDate).NotEmpty().NotNull().WithMessage("Apply date cannot be empty");
+            RuleFor(x => x.Experience).NotEmpty().NotNull().WithMessage("Experience cannot be empty");
+            RuleFor(x => x.TodoOptionsViews).NotNull().WithMessage("Todo options cannot be null");
+        }
+    }
+}
