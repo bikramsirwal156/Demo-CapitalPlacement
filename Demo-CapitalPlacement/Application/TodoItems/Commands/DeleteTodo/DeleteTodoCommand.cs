137095e198f92ae6b09c1bc44bae865007002a9c
Bikram Sirwal <bikram.sirwal@defactoinfotech.com>
2024-05-14 13:23:07 +0530

    Added validator and DeleteToCommand ,UpdateTodoItem implementation

--- Demo-CapitalPlacement/Application/TodoItems/Commands/DeleteTodo/DeleteTodoCommand.cs
+++ Demo-CapitalPlacement/Application/TodoItems/Commands/DeleteTodo/DeleteTodoCommand.cs
@@ -1,6 +1,33 @@
-﻿namespace Demo_CapitalPlacement.Application.TodoItems.Commands.DeleteTodo
+﻿using Demo_CapitalPlacement.Application.Common.Interfaces;
+using Demo_CapitalPlacement.Application.Common.Models;
+using MediatR;
+
+namespace Demo_CapitalPlacement.Application.TodoItems.Commands.DeleteTodo
 {
-    public class DeleteTodoCommand
+    public class DeleteTodoCommand : IRequest<ResultDto<int>>
     {
+        public int Id { get; set; }
+    }
+
+    public class Handler(ICpCodeRepository cpCodeRepository) : IRequestHandler<DeleteTodoCommand, ResultDto<int>>
+    {
+        private readonly ICpCodeRepository _cpCodeRepository = cpCodeRepository;
+
+        public async Task<ResultDto<int>> Handle(DeleteTodoCommand cammand, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await _cpCodeRepository.DeleteTodoItemAsync(cammand.Id);
+                if (result.IsSuccess)
+                {
+                    return new ResultDto<int>(1, true);
+                }
+                return new ResultDto<int>(-1, false);
+            }
+            catch (Exception ex)
+            {
+                return new ResultDto<int>(-1, false, ex.Message);
+            }
+        }
     }
 }
--- Demo-CapitalPlacement/Application/TodoItems/Commands/UpdateTodo/UpdateTodoItemCommand.cs
+++ Demo-CapitalPlacement/Application/TodoItems/Commands/UpdateTodo/UpdateTodoItemCommand.cs
@@ -1,6 +1,75 @@
-﻿namespace Demo_CapitalPlacement.Application.TodoItems.Commands.UpdateTodo
+﻿using Demo_CapitalPlacement.Application.Common.Interfaces;
+using Demo_CapitalPlacement.Application.Common.Models;
+using MediatR;
+
+namespace Demo_CapitalPlacement.Application.TodoItems.Commands.UpdateTodo
 {
-    public class UpdateTodoItemCommand
+    public class UpdateTodoItemCommand : IRequest<ResultDto<int>>
     {
+        public int Id { get; set; }
+        /// <summary>
+        /// Job Description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// This is flag to denote the currenly applying candidate is ex-emp
+        /// if ex-Emp flag is true other mark false
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// This will be a dropwon of yes and no. If the Candidate is refer by some mark yes else no
+        /// </summary>
+        public string RefrenceNumber { get; set; }
+
+        /// <summary>
+        /// Apply Date
+        /// </summary>
+        public DateTime ApplyDate { get; set; }
+
+        /// <summary>
+        /// Experience
+        /// </summary>
+        public int Experience { get; set; }
+
+        public required List<TodoOptionsViewModel> TodoOptionsViews { get; set; }
+
+    }
+
+    public class Handler : IRequestHandler<UpdateTodoItemCommand, ResultDto<int>>
+    {
+        private readonly ICpCodeRepository _cpCodeRepository;
+        public Handler(ICpCodeRepository cpCodeRepository)
+        {
+            _cpCodeRepository = cpCodeRepository;
+        }
+
+        public async Task<ResultDto<int>> Handle(UpdateTodoItemCommand cammand, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var data = new TodoViewModel()
+                {
+                    Id = cammand.Id,
+                    Description = cammand.Description,
+                    ApplyDate = cammand.ApplyDate,
+                    Experience = cammand.Experience,
+                    IsActive = cammand.IsActive,
+                    RefrenceNumber = cammand.RefrenceNumber,
+                    TodoOptionsViewModels = cammand.TodoOptionsViews
+                };
+                var result = await _cpCodeRepository.UpdateTodoItemAsync(data);
+                if (result.IsSuccess)
+                {
+                    return new ResultDto<int>(1, true);
+                }
+                return new ResultDto<int>(-1, false);
+            }
+            catch (Exception ex)
+            {
+                return new ResultDto<int>(-1, false, ex.Message);
+            }
+        }
     }
 }
